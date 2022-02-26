17024c1f92f723f573ede149baf9aeed15be231f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-25 23:42:57 -0700

    m

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -3,13 +3,11 @@
 include(ROOT_PATH . "/app/database/functions.php");
 include(ROOT_PATH . "/app/helpers/middleware.php");
 include(ROOT_PATH . "/app/helpers/validatePost.php");
-session_start();
 
 $table = 'posts';
 
 $topics = selectAll('topics');
 $posts = selectAll($table);
-$users = selectALL('users');
 
 
 $errors = array();
@@ -18,7 +16,6 @@ $title = "";
 $body = "";
 $topic_id = "";
 $published = "";
-$username = "";
 
 if (isset($_GET['id'])) {
     $post = selectOne($table, ['id' => $_GET['id']]);
@@ -28,7 +25,6 @@ if (isset($_GET['id'])) {
     $body = $post['body'];
     $topic_id = $post['topic_id'];
     $published = $post['published'];
-    $username = $post['username'];
 }
 
 if (isset($_GET['delete_id'])) {
@@ -68,7 +64,6 @@ if (isset($_POST['add-post'])) {
         header("location: " . BASE_URL . "/admin/posts/index.php"); 
         exit();    
     } else {
-        $username = $_POST['username'];
         $title = $_POST['title'];
         $body = $_POST['body'];
         $topic_id = $_POST['topic_id'];
@@ -92,7 +87,6 @@ if (isset($_POST['update-post'])) {
         $_SESSION['type'] = "success";
         header("location: " . BASE_URL . "/admin/posts/index.php");       
     } else {
-        $username = $_POST['username'];
         $title = $_POST['title'];
         $body = $_POST['body'];
         $topic_id = $_POST['topic_id'];
