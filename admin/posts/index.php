fd8551f8a648f43f98fe1b6a0bd43c5187048e1d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-26 00:10:56 -0700

    m

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -1,8 +1,7 @@
 <?php 
 include("../../path.php");
 include(ROOT_PATH . "/app/controllers/posts.php");
-include(ROOT_PATH . "/app/controllers/users.php");
-session_start();
+//session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
