072afabf63adabc8d9007a35e1df2428400bc4fc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-25 23:51:58 -0700

    m

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -55,7 +55,6 @@ if (isset($_POST['add-post'])) {
     if (count($errors) == 0) {
         unset($_POST['add-post']);
         $_POST['user_id'] = $_SESSION['id'];
-        $username = $_SESSION['username'];
         $_POST['published'] = isset($_POST['published']) ? 1 : 0;
         $_POST['body'] = htmlentities($_POST['body']);
     
