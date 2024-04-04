291ffee278d01c17be139d7548cf1776cb551d46
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-04 12:57:18 -0500

    update

--- recipes.php
+++ recipes.php
@@ -127,7 +127,7 @@ foreach ($files as $file) {
                       echo $r_category; 
                     ?>
                     <?php
-                    if($r_preheat_temp = null) {
+                    if($r_preheat_temp = NULL) {
                       echo "N/A";
                     } else {
                       echo $r_preheat_temp . '&deg;F'; 
