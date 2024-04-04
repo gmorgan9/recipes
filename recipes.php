23f077bc649fba0d3756111fcf31817eeb584956
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-04 12:45:20 -0500

    update

--- recipes.php
+++ recipes.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Denver');
 require_once "app/database/connection.php";
 // require_once "app/functions/add_app.php";
-require_once "path.php";
+// require_once "path.php";
 session_start();
 
 // $files = glob("app/functions/*.php");
